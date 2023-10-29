bf877e0443a8af6921cca442a806d8f1e976ba0b
Stanislav Shurko <stanislav.shurko@coffeecup.app>
2023-10-29 13:37:41 +0100

    Programmatic UI Preparing

---
+++ CardWorkout/CWButton.swift
@@ -0,0 +1,20 @@
+//
+//  CWButton.swift
+//  CardWorkout
+//
+//  Created by Stanislav Shurko on 29.10.23.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -9,48 +9,10 @@ import UIKit
 
 class CardSelectionVC: UIViewController {
 
-    @IBOutlet var cardImageView: UIImageView!
-    
-    @IBOutlet var buttons: [UIButton]!
-    
-    var cards: [UIImage] = Card.allValues;
-    
-    var timer: Timer!;
-    
     override func viewDidLoad() {
-        super.viewDidLoad();
-        startTimer();
-        
-        for button in buttons {
-            button.layer.cornerRadius = 8;
-        }
-        
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated);
-        
-        timer.invalidate();
-    }
-    
-    func startTimer() {
-        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
-    }
-    
-    @objc func showRandomImage() {
-        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS");
+        super.viewDidLoad()
+
+        view.backgroundColor = .green;
     }
 
-    @IBAction func stopButtonTapped(_ sender: UIButton) {
-        timer.invalidate();
-    }
-    
-    @IBAction func restartButtonTapped(_ sender: Any) {
-        timer.invalidate();
-        startTimer();
-    }
-    
-    @IBAction func unwindHome(_ segue: UIStoryboardSegue) {
-            // this is intentionally blank
-        }
 }

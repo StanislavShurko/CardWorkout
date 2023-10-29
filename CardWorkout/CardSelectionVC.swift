3dd83852768d6598931c652deaa8901ec96dcb2e
Stanislav Shurko <stanislav.shurko@coffeecup.app>
2023-10-29 13:07:55 +0100

    Business Logic added

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -13,24 +13,44 @@ class CardSelectionVC: UIViewController {
     
     @IBOutlet var buttons: [UIButton]!
     
+    var cards: [UIImage] = Card.allValues;
+    
+    var timer: Timer!;
+    
     override func viewDidLoad() {
         super.viewDidLoad();
+        startTimer();
         
         for button in buttons {
             button.layer.cornerRadius = 8;
         }
         
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated);
+        
+        timer.invalidate();
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS");
+    }
 
     @IBAction func stopButtonTapped(_ sender: UIButton) {
-        
+        timer.invalidate();
     }
     
     @IBAction func restartButtonTapped(_ sender: Any) {
-        
+        timer.invalidate();
+        startTimer();
     }
     
-    @IBAction func rulesButtonTapped(_ sender: Any) {
-        
-    }
+    @IBAction func unwindHome(_ segue: UIStoryboardSegue) {
+            // this is intentionally blank
+        }
 }
